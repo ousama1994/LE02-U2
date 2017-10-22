ca4d907c45cc08c7f2fc48be76cd4b5f1cb59b39
Masud Taher <masud.taher@smail.inf.h-brs.de>
2017-10-22 14:44:40 +0200

    author hinzufuegt

--- ProjectTester.java
+++ ProjectTester.java
@@ -1,3 +1,7 @@
+/**
+ * @author Tester: mtaher2s
+ */
+
 package se_02;
 
 import static org.junit.Assert.*;
@@ -6,7 +10,7 @@ import org.junit.Test;
 public class ProjectTester {
 
 	Project seq = new Project(new String[][] { { "A", "B" }, { "C", "D" }, { "B", "C" } });
-	
+
 	@Test
 	public void testOverflow3() {
 		assertTrue(seq.isWellSorted(new String[]{"A", "B", "C", "D"}));
