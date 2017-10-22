c68499526046226388e00afeef930322fcc06518
Ousama EL Emrani <ousama.emrani@smail.inf.h-brs.de>
2017-10-22 14:50:08 +0200

    Project hinzufuegen

---
+++ Project.java
@@ -0,0 +1,18 @@
+package se_02;
+
+public class Project {
+	private String[][] order;
+
+	public Project(String[][] order) {
+		this.order = order;
+	}
+
+	public boolean isWellSorted(String[] seq) {
+		for (int i = 0; i < seq.length - 1; i++) {
+			if (seq[i].compareTo(seq[i + 1]) > 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
